d980b93a1418943a4851a98ac102dd23b463f5e7
evolgenomics <frankyc@tue.mpg.de>
2021-03-18 16:02:43 +0100

    Update setup.sh

--- Simulations/setup.sh
+++ Simulations/setup.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
-for folder in altFa simARG  simBam simFastq simHaps simMuts; do \
-    mkdir $folder; \
-done                                                   
+echo "
+[setup.sh] Creating folder structure...
+
+";
+for folder in sourceData altFa simARG simTrees simBam simFastq simHaps simMuts scripts log; do \
+	mkdir $folder;
+done
+
+echo "
+[setup.sh] Getting and indexing the reference genome assembly...
+
+";
+cd sourceData
+wget http://ftp.tuebingen.mpg.de/fml/ag-chan/haplotagging/simulations/helera1_demo_Herato1603.fa 2&> log/setup.err.log
+bwa index helera1_demo_Herato1603.fa
+samtools faidx helera1_demo_Herato1603.fa
+
+echo "
+[setup.sh] Downloading the reference phased VCF and generating the source haplotypes...
+
+";
+wget http://ftp.tuebingen.mpg.de/fml/ag-chan/haplotagging/simulations/PC062_merged_Herato1603.3.45Mb.PL.AD.HAPCUT2.vcf.gz 2&> setup.err.log 
+wget http://ftp.tuebingen.mpg.de/fml/ag-chan/haplotagging/simulations/PC062_merged_Herato1603.3.45Mb.PL.AD.HAPCUT2.vcf.gz.tbi 2&> setup.err.log 
+wget http://ftp.tuebingen.mpg.de/fml/ag-chan/haplotagging/simulations/add.hdr 2&> setup.err.log 
+wget http://ftp.tuebingen.mpg.de/fml/ag-chan/haplotagging/simulations/header_cols.vcf 2&> setup.err.log 
+wget http://ftp.tuebingen.mpg.de/fml/ag-chan/haplotagging/simulations/vcf.header 2&> setup.err.log 
+
+bcftools query -f "[\t%GT]\n" PC062_merged_Herato1603.3.45Mb.PL.AD.HAPCUT2.vcf.gz | sed 's/[|/]/\t/g;s/^\t//' | datamash transpose | sed 's/\t//g' | sort > PC062_merged_Herat1603_3.45Mb.simBlock.horiz.sorted 
+bcftools query -f "%CHROM\t%POS\t%REF\t%ALT\n" PC062_merged_Herato1603.3.45Mb.PL.AD.HAPCUT2.vcf.gz > PC062_merged_Herato1603.3.45Mb.PL.AD.HAPCUT2.pos
+for i in `seq 0 2 967`; do echo "hap_"$i; done > sample.names
+cd ..
