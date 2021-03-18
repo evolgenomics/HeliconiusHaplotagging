b24f8aef0d3c543d49acf3563180c29271360b69
evolgenomics <frankyc@tue.mpg.de>
2021-03-18 16:02:17 +0100

    Create run_all.sh

---
+++ Simulations/run_all.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+tag="example"
+
+echo "***NOTE: The entire pipeline with 968 samples takes a long time to run. Consider running this in a screen or similar virtual terminals.***";
+echo "[run_all.sh] Setting up...
+";
+
+#./setup.sh
+
+echo "[run_mspms.sh] Simulate using mspsm 968 haplotypes, 13505 SNPs, at 10 rho using the tag \"example\"...
+";
+
+#scripts/run_mspms.sh example 13505 968 10 1
+
+echo "[run_all.sh] Generating truth haploytpes...
+";
+
+-TEST#tperl scripts/simulate_haplotypes_ARG.pl $tag 1 968 0.01
+
+echo "[run_all.sh] Starting simulation...
+";
+
+scripts/simulate_reads-molecules.sh $tag
+
+scripts/run_STITCH.sh $tag linkedReads simBam/bamfiles.linkedReads.$tag.968.list --use_bx_tag=TRUE  
+scripts/run_STITCH.sh $tag shortReads simBam/bamfiles.linkedReads.$tag.968.list
+
+for datatype in linked short; do 
+	scripts/post_STITCH.sh STITCH_out/STITCH_968_${datatype}Reads_$tag; 
+	scripts/run_hapcut2.sh STITCH_out/STITCH_968_${datatype}Reads_$tag; 
+done; 
